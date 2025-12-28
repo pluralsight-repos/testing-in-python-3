4702f42fcc5e6e02c9292cc5be7c4f6021e9ba11
msg.edson <msg.edson@gmail.com>
2025-12-28 10:29:00 -0300

    Implement Phonebook class with add and lookup methods; add unit tests for functionality

--- phonebook/test_phonebook.py
+++ phonebook/test_phonebook.py
@@ -6,3 +6,12 @@ class PhonebookTest(unittest.TestCase):
     def test_lookup_by_name(self):
         phonebook = Phonebook()
         phonebook.add("Bob", "12345")
+
+        number = phonebook.lookup("Bob")
+
+        self.assertEqual(number, "12345")
+
+    def test_missing_name(self):
+        phonebook = Phonebook()
+        with self.assertRaises(KeyError):
+            phonebook.lookup("missing!")
