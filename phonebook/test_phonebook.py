2c2f1792e10785e611bff656e1429c0a61f3ba1c
msg.edson <msg.edson@gmail.com>
2025-12-28 11:43:47 -0300

    Refactor PhonebookTest to use instance variable for Phonebook instance

--- phonebook/test_phonebook.py
+++ phonebook/test_phonebook.py
@@ -3,21 +3,22 @@ import unittest
 from phonebook import Phonebook
 
 class PhonebookTest(unittest.TestCase):
+    
+    def setUp(self) -> None:
+        self.phonebook = Phonebook()
+    
     def test_lookup_by_name(self):
-        phonebook = Phonebook()
-        phonebook.add("Bob", "12345")
+        self.phonebook.add("Bob", "12345")
 
-        number = phonebook.lookup("Bob")
+        number = self.phonebook.lookup("Bob")
 
         self.assertEqual(number, "12345")
 
     def test_missing_name(self):
-        phonebook = Phonebook()
         with self.assertRaises(KeyError):
-            phonebook.lookup("missing!")
+            self.phonebook.lookup("missing!")
 
     @unittest.skip("Work in progress")
     def test_empty_phonebook_is_consistent(self):
-        phonebook = Phonebook()
-        is_consistent = phonebook.is_consistent()
+        is_consistent = self.phonebook.is_consistent()
         self.assertTrue(is_consistent)
