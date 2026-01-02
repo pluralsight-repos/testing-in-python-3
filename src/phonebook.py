c36d33c0081c64459853ecf3f15898c79bc090fe
msg.edson <msg.edson@gmail.com>
2026-01-02 15:30:32 -0300

    parameterized testing

--- src/phonebook.py
+++ src/phonebook.py
@@ -21,12 +21,12 @@ class Phonebook:
         self.cache.close()
         os.remove(self.filename)    
     
-    # def is_consistent(self):
-    #     for name1, number1 in self.numbers.items():
-    #         for name2, number2 in self.numbers.items():
-    #             if name1 == name2:
-    #                 continue
-    #             if number1.startswith(number2):
-    #                 return False
-    #     return True
+    def is_consistent(self):
+        for name1, number1 in self.numbers.items():
+            for name2, number2 in self.numbers.items():
+                if name1 == name2:
+                    continue
+                if number1.startswith(number2):
+                    return False
+        return True
     
--- tests/test_phonebook.py
+++ tests/test_phonebook.py
@@ -24,4 +24,31 @@ def test_missing_name(phonebook):
     # phonebook = Phonebook()
     with pytest.raises(KeyError):
         phonebook.lookup("Bob")
+
+def test_is_consistent_with_different_method(phonebook):
+    phonebook.add("Bob","12345")
+    phonebook.add("Anna","01234")
+    assert phonebook.is_consistent()
     
+def test_inconsistent_with_duplicate_method(phonebook):
+    phonebook.add("Bob","12345")
+    phonebook.add("Sue","12345")
+    assert not phonebook.is_consistent()
+    
+def test_inconsistent_with_duplicate_prefix(phonebook):
+    phonebook.add("Bob","12345")
+    phonebook.add("Sue","123")
+    assert not phonebook.is_consistent()
+
+@pytest.mark.parametrize(
+    "entry1,entry2,is_consistent", [
+        (("Bob","12345"),("Anna","01234"), True),
+        (("Bob","12345"),("Sue","12345"), False),
+        (("Bob","12345"),("Sue","123"), False),
+    ]
+)
+
+def test_is_consistent(phonebook, entry1, entry2, is_consistent):
+    phonebook.add(*entry1)
+    phonebook.add(*entry2)
+    assert phonebook.is_consistent() == is_consistent
