09dd7568593182a3315f6a3a543db9125a78a2ab
msg.edson <msg.edson@gmail.com>
2026-01-02 11:16:06 -0300

    add fixture

--- tests/test_phonebook.py
+++ tests/test_phonebook.py
@@ -1,13 +1,25 @@
+import pytest
 from phonebook import Phonebook
 
-def test_lookup_by_name():
+@pytest.fixture
+def phonebook():
     phonebook = Phonebook()
+    return phonebook
+
+def test_lookup_by_name(phonebook):
+    # phonebook = Phonebook()
     phonebook.add("Bob", "12345")
     number = phonebook.lookup("Bob")
     assert number == "12345"
     
-def test_contains_all_names():
-    phonebook = Phonebook()
+def test_contains_all_names(phonebook):
+    # phonebook = Phonebook()
     phonebook.add("Bob", "12345")
-    assert "Missing" in phonebook.all_names()
-    assert phonebook.all_names() == {"Bob", "Missing"}
+    assert "Bob" in phonebook.all_names()
+    assert phonebook.all_names() == {"Bob"}
+    
+def test_missing_name(phonebook):
+    # phonebook = Phonebook()
+    with pytest.raises(KeyError):
+        phonebook.lookup("Bob")
+    
